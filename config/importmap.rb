b6570b2751988d23791e59ce1acae9a5c3b630ce
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-01-02 22:29:06 +0100

    add local time to posts

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "localtime", to: "https://ga.jspm.io/npm:localtime@1.0.2/lib/localtime.js"
